49f19664549f18779aa5ab3251f47e46218d4e65
Ryan Hammonds <ryanphammonds@gmail.com>
2025-01-30 17:45:47 -0800

    add overwrite option to fetch_data

--- src/neurovlm/data.py
+++ src/neurovlm/data.py
@@ -7,38 +7,43 @@ import requests
 
 URL = "https://github.com/neurovlm/neurovlm_data/raw/refs/heads/main/data"
 
-def fetch_data(url:Optional[str]=URL):
+def fetch_data(url: Optional[str]=URL, overwrite: Optional[bool]=False) -> str:
     """Fetch data from a url.
 
     Parameters
     ----------
     url : string, optional, default: URL
         URL to data.
+    overwrite : bool, optional, default: False
+        Overwrite existing files.
 
     Returns
     -------
     save_dir : string
         Path to saved data.
     """
-    files = ["mask.npz"]
+    files = ["mask.npz", "publications.parquet", "coordinates.parquet"]
 
     save_dir = os.path.dirname(os.path.realpath(__file__))
     if "neurovlm_data" not in os.listdir(save_dir):
         os.mkdir(save_dir + "/neurovlm_data")
+    elif not overwrite:
+        return save_dir + "/neurovlm_data"
+
     save_dir = save_dir + "/neurovlm_data"
 
     for f in files:
 
-        # Construct the full URL for the file
+        # File url
         file_url = f"{url}/{f}"
 
-        # Fetch the file
+        # Fetch
         response = requests.get(file_url)
 
         if response.status_code == 200:
-            # Save the file to the 'data' directory
+            # Save file
             with open(f"{save_dir}/{f}", "wb") as file:
-                file.write(response.content)  # Write binary data to file
+                file.write(response.content)
         else:
             print(f"Error fetching '{f}': {response.status_code}")
 
